c13b3cf11802dac15d4240ebbfb45e964034a0e0
Abhay <me.ydv.abhay@gmail.com>
2022-11-24 23:00:16 +0530

    my second

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
+
+
+const moviesUrl = "https://www.omdbapi.com/?apikey=45f0782a&s=war"
+let picture=[]
+
+function App() {
+  const [movies,setPosts] = useState(null)
+  const [searchMov,setSearchMov] = useState('')
+
+  useEffect(() => {
+    axios.get(moviesUrl).then(response => {
+      console.log(response.data.Search)
+      setPosts(response.data.Search)
+      picture = response.data.Search
+    })
+  },[])
+
+  const pictureSearch = (e) => {
+    if (movies) {
+      setPosts(picture.filter(movie => movie.Title.toLowerCase().includes(e)))
+    }
+  }
+  
+  return (
+    <div className="App">
+      <br/>
+          <input type="text" placeholder='search for Movie Title ...' onChange={(e) => pictureSearch(e.target.value.toLowerCase())} />
+          <br/>
+       {movies.map((movie,i)=> <div >
+           <img src={movie.Poster}></img>
+           <p>{movie.Title}</p>
+         </div>
+)
+     }
+    </div>
+  );
+}
+
+export default App;
